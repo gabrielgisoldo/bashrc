cd1a71589c5323b30f50a0b3453e781cb5880940
gabriel diniz gisoldo <gabrielgisoldi@gmail.com>
2018-03-28 14:41:49 -0300

    Changed all the string to english

--- update_slack.sh
+++ update_slack.sh
@@ -1,49 +1,61 @@
-echo -e "Os comandos serão executados como root\n\n"
+echo -e "All commands will be executed as root\n\n"
 
-# INICIO: Atualizar os pacotes da distro usando o slackpkg
-read -p "Atualizar pacotes do rep. Slackware 14.2 (s/n): " op
+slackpkgplus=$(ls /var/log/packages | grep slackpkg+)
+sbotools=$(ls /var/log/packages | grep sbotools)
+slkupdt="0"
+slkupdtgpg="0"
+
+# START: Update distro packages using slackpkg
+read -p "Update packages from distro (s/n): " op
 if [ "$op" == "s" ]; then
+    if [ ${#slackpkgplus} -gt 0 ]; then
+        su - root -c "slackpkg update gpg"
+        slkupdtgpg="1"
+    fi
+    slkupdt="1"
     su - root -c "slackpkg update"
     su - root -c "slackpkg install-new"
     su - root -c "slackpkg upgrade-all"
 fi
-# FIM: Atualizar os pacotes da distro usando o slackpkg
+# END: Update distro packages using slackpkg
 
 echo
 
-# INICIO: Atualizar os pacotes de 32 bits
-read -p "Atualizar pacotes 32 bits (s/n): " op
+# START: Update 32bits distro packages
+read -p "Update 32 bits packages(s/n):" op
 if [ "$op" == "s" ]; then
-    su - root -c "massconvert32.sh -u http://someserver.org/path/to/slackware-14.2/slackware"
+    su - root -c "massconvert32.sh -u REPOSITORIO_SLACK"
     su - root -c "upgradepkg --install-new *-compat32/*.t?z"
     su - root -c "rm -rf *-compat32"
 fi
-# FIM: Atualizar os pacotes de 32 bits
+# END: Update 32bits distro packages
 
 echo
 
-# INICIO: Atualizar pacotes usando SBO
-read -p "verificar pacotes com o SBO(s/n): " op
-if [ "$op" == "s" ]; then
-    su - root -c "sbocheck"
-    pkg=$(cat /var/log/sbocheck.log | awk '{print $1}' | tr '\n' ' ')
-    echo
-    if [ ${#pkg} -gt 0 ]; then 
-        read -p "Atualizar todos os pacotes (s/n): " att
-        if [ "$att" == "s" ]; then
-            su - root -c "sboupgrade $pkg"
-        else
-            echo -e "\nPacotes para atualizar:\n\n${pkg}\n"
-            read -p "Digite o nome do pacote ou . para sair: " pacote
-            if [ "$pacote" != "." ]; then
-                su - root -c "sboupgrade $pacote"
+# START: Update packages using SBO
+if [ ${#sbotools} -gt 0 ]; then
+    read -p "Check packages to update with SBO(s/n): " op
+    if [ "$op" == "s" ]; then
+        su - root -c "sbocheck"
+        pkg=$(cat /var/log/sbocheck.log | awk '{print $1}' | tr '\n' ' ')
+        echo
+        if [ ${#pkg} -gt 0 ]; then 
+            read -p "Update all packages (s/n): " att
+            if [ "$att" == "s" ]; then
+                su - root -c "sboupgrade $pkg"
             else
-                echo
+                echo -e "\nPackages to update:\n\n${pkg}\n"
+                read -p "Which packages to update(type '.' to exit): " pacote
+                if [ "$pacote" != "." ]; then
+                    su - root -c "sboupgrade $pacote"
+                else
+                    echo
+                fi
             fi
+            su - root -c "> /var/log/sbocheck.log"
+        else
+            echo "There is no package to update."
         fi
-        su - root -c "> /var/log/sbocheck.log"
-    else
-        echo "Nenhum pacote para atualizar."
     fi
 fi
-# FIM: Atualizar pacotes usando SBO
+# END: Update packages using SBO
