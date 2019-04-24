81064e19da5fe404f10090ebcc0ee354590ac34e
gabriel diniz gisoldo <gabrielgisoldi@gmail.com>
2019-04-24 15:25:06 -0300

    update to use sudo in place of su -c

--- update_slack.sh
+++ update_slack.sh
@@ -1,21 +1,22 @@
-echo -e "All commands will be executed as root\n\n"
+echo -e "All commands will be executed with sudo\n\n"
 
 slackpkgplus=$(ls /var/log/packages | grep slackpkg+)
 sbotools=$(ls /var/log/packages | grep sbotools)
 slkupdt="0"
 slkupdtgpg="0"
+REPO_SLACK=""
 
 # START: Update distro packages using slackpkg
 read -p "Update packages from distro (s/n): " op
 if [ "$op" == "s" ]; then
     if [ ${#slackpkgplus} -gt 0 ]; then
-        su - root -c "slackpkg update gpg"
+        sudo /usr/sbin/slackpkg update gpg
         slkupdtgpg="1"
     fi
     slkupdt="1"
-    su - root -c "slackpkg update"
-    su - root -c "slackpkg install-new"
-    su - root -c "slackpkg upgrade-all"
+    sudo /usr/sbin/slackpkg update
+    sudo /usr/sbin/slackpkg install-new
+    sudo /usr/sbin/slackpkg upgrade-all
 fi
 # END: Update distro packages using slackpkg
 
@@ -24,9 +25,9 @@ echo
 # START: Update 32bits distro packages
 read -p "Update 32 bits packages(s/n):" op
 if [ "$op" == "s" ]; then
-    su - root -c "massconvert32.sh -u REPOSITORIO_SLACK"
-    su - root -c "upgradepkg --install-new *-compat32/*.t?z"
-    su - root -c "rm -rf *-compat32"
+    sudo /usr/sbin/massconvert32.sh -u "${REPO_SLACK}"
+    sudo /sbin/upgradepkg --install-new *-compat32/*.t?z
+    sudo find /tmp/ -name '*-compat32' -maxdepth 1 -print | xargs sudo /usr/bin/rm -rf
 fi
 # END: Update 32bits distro packages
 
@@ -36,23 +37,25 @@ echo
 if [ ${#sbotools} -gt 0 ]; then
     read -p "Check packages to update with SBO(s/n): " op
     if [ "$op" == "s" ]; then
-        su - root -c "sbocheck"
+        sudo /usr/sbin/sbocheck
         pkg=$(cat /var/log/sbocheck.log | awk '{print $1}' | tr '\n' ' ')
         echo
         if [ ${#pkg} -gt 0 ]; then 
             read -p "Update all packages (s/n): " att
             if [ "$att" == "s" ]; then
-                su - root -c "sboupgrade $pkg"
+                sudo /usr/sbin/sboupgrade "${pkg}"
             else
                 echo -e "\nPackages to update:\n\n${pkg}\n"
                 read -p "Which packages to update(type '.' to exit): " pacote
                 if [ "$pacote" != "." ]; then
-                    su - root -c "sboupgrade $pacote"
+                    sudo /usr/sbin/sboupgrade "${pacote}"
                 else
                     echo
                 fi
             fi
-            su - root -c "> /var/log/sbocheck.log"
+            sudo /usr/sbin/sboclean -w -d
+            sudo /usr/bin/rm -f /var/log/sbocheck.log
+            sudo touch /var/log/sbocheck.log
         else
             echo "There is no package to update."
         fi
